1fb51d451c2b8d238064844e02025a6cd9aa7777
Kristijan Trajkovski <kiko_trajkovski@hotmail.com>
2019-02-22 14:22:18 +0100

    Initial Commit

---
+++ GKNativeExtensions/GKNativeExtensions.h
@@ -0,0 +1,19 @@
+//
+//  GKNativeExtensions.h
+//  GKNativeExtensions
+//
+//  Created by Kristijan Trajkovski on 2/22/19.
+//  Copyright © 2019 Kristijan Trajkovski. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GKNativeExtensions.
+FOUNDATION_EXPORT double GKNativeExtensionsVersionNumber;
+
+//! Project version string for GKNativeExtensions.
+FOUNDATION_EXPORT const unsigned char GKNativeExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GKNativeExtensions/PublicHeader.h>
+
+
