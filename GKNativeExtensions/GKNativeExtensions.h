788d64f652c9a5e558b6b8e3b2620756d3afbc19
Kristijan Trajkovski <kiko_trajkovski@hotmail.com>
2019-03-13 13:58:20 +0100

    Updated license

--- GKNativeExtensions/GKNativeExtensions.h
+++ GKNativeExtensions/GKNativeExtensions.h
@@ -1,10 +1,7 @@
-//
-//  GKNativeExtensions.h
-//  GKNativeExtensions
-//
-//  Created by Kristijan Trajkovski on 2/22/19.
-//  Copyright © 2019 Kristijan Trajkovski. All rights reserved.
-//
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 #import <Foundation/Foundation.h>
 
 #ifdef __cplusplus
